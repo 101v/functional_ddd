6a2b919b480b01c9fe3f3390165cdbee0d18ddb1
Vimal Patel <vp@123insight.com>
2018-10-08 19:26:37 +0100

    added types

--- src/OrderManagement/OrderTaking.Domain.fs
+++ src/OrderManagement/OrderTaking.Domain.fs
@@ -1,17 +1,31 @@
 namespace OrderTaking.Domain
 
 module Order =
-    type WidgetCode = WidgetCode of string
+    type WidgetCode = private WidgetCode of string
+    module WidgetCode =
+        let create (code : string) =
+            match code with
+            | c when c.StartsWith("W") -> Ok(WidgetCode c)
+            | c -> Error("Invalid widget code")
+        let valud (WidgetCode code) = code
+    
     type GizmoCode = GizmoCode of string
     type ProductCode =
         | Widget of WidgetCode
         | Gizmo of GizmoCode
 
-    type UnitQuantity = UnitQuantity of int
+    type UnitQuantity = private UnitQuantity of int
     type KilogramQuantity = KilogramQuantity of decimal
     type OrderQuantity = 
         | Unit of UnitQuantity
         | Kilos of KilogramQuantity
+
+    module UnitQuantity = 
+        let create qty =
+            if qty < 1 then Error "UnitQuantity can not be negative"
+            else if qty > 1000 then Error "UnitQuantity can not be more than 1000"
+            else Ok(UnitQuantity qty)
+        let value (UnitQuantity qty) = qty
 
     type Undefined = exn
     type OrderId = Undefined
@@ -24,12 +38,19 @@ module Order =
     type Price = Undefined
     type BillingAmount = Undefined
 
+    type NonEmptyList<'a> = {
+        First: 'a
+        Rest: 'a list
+    }
+
+    let l : NonEmptyList<int> = {First = 1; Rest = []}
+
     type Order = {
         Id : OrderId
         CustomerId : CustomerId
         ShippingAddress : ShippingAddress
         BillingAddress : BillingAddress
-        OrderLines : OrderLine list
+        OrderLines : NonEmptyList<OrderLine>
         AmountToBill : BillingAmount
     }
 
@@ -61,3 +82,6 @@ module Order =
 
     type PlaceOrder = 
         UnvalidateOrder -> Result<PlaceOrderEvents, PlaceOrderError>
+    
+    type AddressValidationService = 
+        UnvalidateOrder -> PlaceOrderEvents option
